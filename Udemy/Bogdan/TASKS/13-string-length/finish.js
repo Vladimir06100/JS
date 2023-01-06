d10711c5ec082d4fbcff7afe7029e400be50f89b
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-06 22:51:40 +0100

    string length

--- Udemy/Bogdan/TASKS/13-string-length/finish.js
+++ Udemy/Bogdan/TASKS/13-string-length/finish.js
@@ -2,5 +2,9 @@
  *
  * 1. Создайте переменную и присвойте ей любую строку
  *
- * 2. Виведите в консоль длину этой строки
+ * 2. Выведите в консоль длину этой строки
  */
+
+const myFavoriteSport = 'Snowboarding'
+const stringLength = myFavoriteSport.length
+console.log(stringLength)
