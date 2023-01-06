77cb902ce5399c5a36089b73dcbbaa112e1c2664
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-06 19:32:26 +0100

    typeof

--- Udemy/Bogdan/TASKS/06-data-types/finish.js
+++ Udemy/Bogdan/TASKS/06-data-types/finish.js
@@ -12,3 +12,22 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+const myString = 'abc'
+const myNumber = 10
+const myBoolean = true
+const myNull = null
+const myUndefined = undefined
+// soit let myUndefined
+const myObject = {}
+const myArray = []
+// typeof унарный оператор
+console.log(typeof myString)
+console.log(typeof myNumber)
+console.log(typeof myBoolean)
+// значение создано для создания пустого значения объекта
+console.log(typeof myNull)
+console.log(typeof myUndefined)
+// 
+console.log(typeof myObject)
+// массив так же является объектом
+console.log(typeof myArray)
