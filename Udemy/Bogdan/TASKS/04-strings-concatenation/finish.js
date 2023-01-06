55d4e3222c8c28849bd718bd0038248e43b6dd32
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-06 13:18:34 +0100

    4

--- Udemy/Bogdan/TASKS/04-strings-concatenation/finish.js
+++ Udemy/Bogdan/TASKS/04-strings-concatenation/finish.js
@@ -1,3 +1,4 @@
+"use strict";
 /** ЗАДАЧА 4 - Объединение строк
  *
  * 1. Объявите три переменные с значениями:
@@ -10,3 +11,15 @@
  *
  * 3. Выведите значение последней переменной в консоль
  */
+let myName = 'Vladimir'
+let mySurname = 'Sinkevitch'
+let myProfession = 'developer'
+
+const personInfo = 'my name is ' + myName + ' ' + mySurname + ' and I am ' + myProfession
+
+console.log(personInfo)
+// option 2
+
+const personInfo2 = `My name is ${myName} ${mySurname} and I am ${myProfession}`
+console.log(personInfo2)
+
