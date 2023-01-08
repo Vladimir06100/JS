6b16ca0f7d6511064a7459c89466fa406d98f355
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-08 21:04:12 +0100

    .

--- Udemy/Bogdan/TASKS/23-or-and-operators/finish.js
+++ Udemy/Bogdan/TASKS/23-or-and-operators/finish.js
@@ -4,3 +4,12 @@
  */
 
 console.log(3 || (true && null) || false)
+
+// шаг 1 получаем true && null = null
+// шаг 2 3 || null || false = 3
+
+
+// или выведено правда
+
+
+console.log(true && 'abc' && 10) // 10
