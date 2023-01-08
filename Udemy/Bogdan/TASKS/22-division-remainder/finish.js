344b5e2d8a5aba25c46f5390e606c65551304858
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-08 20:35:58 +0100

    .

--- Udemy/Bogdan/TASKS/22-division-remainder/finish.js
+++ Udemy/Bogdan/TASKS/22-division-remainder/finish.js
@@ -10,3 +10,7 @@
 
 const myNumber1 = 10 // 10 = 3 + 3 + 3 + 1
 const myNumber2 = 3
+// остаток от деления Reminder 1
+console.log(myNumber1 % myNumber2) 
+// возведение в степень ** 
+console.log (100 % 23 % 5 )
