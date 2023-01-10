36ffbec3703db763945d89fa3f0ebcdbf75b45f9
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-10 21:37:03 +0100

    .

--- Udemy/Bogdan/TASKS/41-find-single-post/finish.js
+++ Udemy/Bogdan/TASKS/41-find-single-post/finish.js
@@ -18,6 +18,10 @@ const posts = [
   { postId: 6134, commentsQuantity: 2 },
 ]
 
+function findPostById(postId, posts) {
+  return posts.find((post)=> post.postId === postId)
+}
+
 console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
 
 console.log(findPostById(4511, posts)) // undefined
