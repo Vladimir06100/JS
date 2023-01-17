e025016b1dab463fbe863975b249511623c9fe66
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-17 22:23:43 +0100

    .

--- Udemy/Bogdan/TASKS/59-extend-array/finish.js
+++ Udemy/Bogdan/TASKS/59-extend-array/finish.js
@@ -13,3 +13,27 @@
  * 4. Убедитесь, что остальные методы массивов такие,
  * как "forEach", "map" также доступны
  */
+
+class ExtendedArray extends Array {
+  sum() {
+    return this.reduce((sum, num) => sum + num, 0);
+  }
+
+  onlyNumbers() {
+    return this.filter((el) => typeof el === 'number');
+  }
+}
+
+const myExtendedArray = new ExtendedArray(10, 4, 5);
+console.log(myExtendedArray);
+
+console.log(myExtendedArray.sum());
+
+const myExtendedArray2 = new ExtendedArray('abc', 5, true, 25);
+console.log(myExtendedArray2.onlyNumbers());
+
+myExtendedArray2.forEach((el) => console.log(el));
+
+// // Array instances don't have access to methods sum and onlyNumbers
+// const myArray = [1, 2, 3]
+// myArray.sum()
