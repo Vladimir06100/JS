3739930be005e1dc29b327a47d65f846e6ec1754
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2023-01-21 22:27:50 +0100

    ....

--- JS-training/13_Resto_Commande/script.js
+++ JS-training/13_Resto_Commande/script.js
@@ -7,14 +7,14 @@ const ba = 8;
 let age = prompt("Quel âge avez-vous ?");
 age = parseInt(age); // Il faut transformer l'âge en nombre si on veut faire des calculs (Pas toujours fais automatiquement)
 let credit = prompt("Combien d'argent disposez-vous ?");
-credit = parseInt(credit); // Il faut transformer le crédit  en nombre si on veut pouvoir faire des calculs (Pas toujours automatique)
-let choix = ""; // Choix vide ici pour pas que le chois soit undefined dans la boucle
+credit = parseInt(credit); // Il faut transformer le crédit en nombre si on veut pouvoir faire des calculs (Pas toujours automatique)
+let choix = ""; // Choix vide ici pour pas que le choie soit undefined dans la boucle
 
 // Fonction qui affiche le menu en fonction du crédit disponible et de l'âge pour l'alcool
 function menu1(credit) {
 	let result = ""; // On part d'un result vide
 
-	result = result + "Veuillez choisir votre commande \n "; // Peu importe le crédit on aura toujours cette phrase dans result
+	result = result + "Veuillez choisir votre commande \n "; // Peu importe le crédit, on aura toujours cette phrase dans result
 
 	if (credit >= 20) {
 		result = result + "1 - Plat (20€)\n"; // On ajoute à result le plat
@@ -27,7 +27,7 @@ function menu1(credit) {
 	}
 
 	result =
-		result + "4 - Sortir \n --------------- \n Votre crédit est de: " + credit; // Peu importe le crédit on aura toujours cette phrase dans result
+		result + "4 - Sortir \n --------------- \n Votre crédit est de: " + credit; // Peu importe le crédit, on aura toujours cette phrase dans result
 
 	return result;
 }
@@ -38,7 +38,7 @@ function buyProduct(price, credit) {
 	if (credit >= price) {
 		credit = credit - price; // Soustraction du prix dans le crédit
 		alert("Bon appétit il vous reste: " + credit); // Affichage de confirmation de commande
-		return credit; // On renvoi le nouveau crédit
+		return credit; // On est renvoi le nouveau crédit
 	} else {
 		error(); // Affichage du message d'erreur
 	}
@@ -51,26 +51,26 @@ function error() {
 
 // POINT VIRGULE A CHAQUE FIN D'INSTRUCTION EN JS;
 
-// TANT QUE le crédit est supérieur ou égal à la BNA (5) et que le choix est différent de 4
-while (credit >= bna && choix != 4) {
+// TANT QUE le crédit est supérieur ou égal à la BNA (5) et que le choix est différent de
+while (credit >= bna && choix !== 4) {
 	// CREATION DES CONDITIONS
 
 	let menu = menu1(credit); // Appel de la fonction menu avec pour paramètre crédit
 
 	choix = prompt(menu); // Affichage du menu en fonction du return de la fonction "menu"
 
-	if (choix == 1) {
-		// credit prend la valeur du  return de la fonction buyProduct
+	if (choix === 1) {
+		// credit prend la valeur du return de la fonction buyProduct
 		credit = buyProduct(plat);
-	} else if (choix == 2) {
+	} else if (choix === 2) {
 		// credit prend la valeur du return de la fonction buyProduct
 		credit = buyProduct(bna);
-	} else if (choix == 3) {
-		// credit prend la valeur du  return de la fonction buyProduct
+	} else if (choix === 3) {
+		// credit prend la valeur du return de la fonction buyProduct
 		credit = buyProduct(ba);
 	} else {
-		// Si le choix est == à 4 on envoi un message d'au revoir
-		if (choix == 4) {
+		// Si le choix est == à 4 ont envoi un message d'au revoir
+		if (choix === 4) {
 			alert("A bientôt");
 		} else {
 			alert("Je n'ai pas compris votre choix réessayer"); // Choix incompris par l'algorithme
